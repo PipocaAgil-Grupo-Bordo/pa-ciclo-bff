1ee2a9a79d82eef98c38a281b025993fed27f390
Luciano Martins <zicadopv@gmail.com>
2024-02-29 09:27:24 -0300

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+// import { ConfigModule } from '@nestjs/config';
+import { UsuarioModule } from './usuario/usuario.module';
+
+@Module({
+  imports: [UsuarioModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
