bd181f0a8dc65e5a8465fb2debd65dd30797a829
= <=>
2024-03-03 13:52:29 -0300

    test with no encryption service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { EncryptionModule } from 'src/shared/services/encryption/encryption.module';
+// import { EncryptionModule } from 'src/shared/services/encryption/encryption.module';
 import { UserModule } from '../user/user.module';
 import { TokenModule } from 'src/shared/services/token/token.module';
 
 @Module({
-  imports: [EncryptionModule, TokenModule, UserModule],
+  // imports: [EncryptionModule, TokenModule, UserModule],
+  imports: [TokenModule, UserModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
