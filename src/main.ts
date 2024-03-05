771faefb0e8a3b1f7825bb4bd4e086cbcb7bdaf6
= <=>
2024-03-05 14:43:38 -0300

    feat: generate simple docs version

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { swaggerConfig } from './swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Ciclo Ágil API')
+    .setDescription('Documentação da API do projeto Ciclo Ágil.')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .addTag('auth', 'Autenticação de usuários')
+    .build();
+
   const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, swaggerDoc);
   await app.listen(3000);
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiCreatedResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @ApiCreatedResponse({ type: LoginResponseDto })
   @Post('login')
   login(@Body() body: LoginDto) {
     return this.authService.login(body);
