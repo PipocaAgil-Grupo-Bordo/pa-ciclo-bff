b9dcd28f2f2748c6f3883a33c55f402bd1204ce3
= <=>
2024-03-04 21:39:12 -0300

    add swagger module to setup and generate the api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Ciclo Ágil API')
+    .setDescription('Documentação da API do projeto Ciclo Ágil.')
+    .setVersion('1.0.0')
+    .build();
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDoc);
   await app.listen(3000);
 }
 bootstrap();
