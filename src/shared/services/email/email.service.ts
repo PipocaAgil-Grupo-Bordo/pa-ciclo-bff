62c6aa36ca537bbf67a08e4aa3cf4e7afbef2e40
= <=>
2024-03-15 17:54:23 -0300

    Add some stylying adjustments to the email body

--- src/shared/services/email/email.service.ts
+++ src/shared/services/email/email.service.ts
@@ -29,17 +29,17 @@ export class EmailService {
           <title>Redefinição de Senha</title>
         </head>
         <body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
-          <div style="background-color: #f5f5f5; padding: 20px;">
+          <div style="background-color: #f5f5f5; padding: 50px;">
             <div style="max-width: 600px; margin: 0 auto; background-color: #fff; border-radius: 10px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
               <div style="padding: 20px;">
                 <h2 style="color: #333; margin-bottom: 20px;">Redefinição de Senha</h2>
-                <p>Olá <strong style="color: #007bff;">${user.name}</strong>,</p>
+                <p>Olá <strong style="color: #8c36c7;">${user.name}</strong>,</p>
                 <p>Recebemos uma solicitação de redefinição de senha para sua conta. Utilize o código de verificação abaixo para concluir o processo:</p>
-                <div style="background-color: #007bff; color: #fff; padding: 10px; border-radius: 5px; margin-top: 20px; text-align: center;">
+                <div style="background-color: #8c36c7; color: #fff; padding: 10px; border-radius: 5px; margin-top: 20px; text-align: center;">
                   <h3 style="margin: 0; font-size: 24px;">${code}</h3>
                 </div>
                 <p style="margin-top: 20px;">Se você não solicitou essa redefinição de senha, pode ignorar este e-mail com segurança. Este link é válido por 1 hora.</p>
-                <p style="margin-top: 20px;">Atenciosamente,<br><strong style="color: #007bff;">Equipe Ciclo Ágil</strong></p>
+                <p style="margin-top: 20px;">Atenciosamente,<br><strong style="color: #8c36c7;">Equipe Ciclo Ágil</strong></p>
               </div>
             </div>
           </div>
