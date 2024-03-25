4ac12cae5ef48152338d0c208efb1541ed4fef3c
= <=>
2024-03-25 18:31:53 -0300

    add email verification before validatin the code

--- src/modules/verification-code/verification-code.module.ts
+++ src/modules/verification-code/verification-code.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { VerificationCodeRepository } from './verification-code.repository';
 import { VerificationCodeService } from './verification-code.service';
+import { UserModule } from '../user/user.module';
+import { UserService } from '../user/user.service';
 
 @Module({
   controllers: [],
+  imports: [UserModule],
   exports: [VerificationCodeService],
-  providers: [VerificationCodeService, VerificationCodeRepository],
+  providers: [VerificationCodeService, VerificationCodeRepository, UserService],
 })
 export class VerificationCodeModule {}
