568f1e895618401c213266d97d825a746b26d717
= <=>
2024-03-25 18:22:47 -0300

    add code max length

--- src/modules/auth/dtos/verification-code-validation.dto.ts
+++ src/modules/auth/dtos/verification-code-validation.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 export class VerificationCodeValidationDto {
   @IsNotEmpty()
   @IsString()
-  @Length(6)
+  @Length(6, 6)
   code: string;
 
   @IsNotEmpty()
