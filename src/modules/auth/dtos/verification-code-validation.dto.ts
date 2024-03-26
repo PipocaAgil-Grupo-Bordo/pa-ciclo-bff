1ddffa564311f350da6dec542e39dd4f2a7b8031
= <=>
2024-03-26 12:57:07 -0300

    fix code length error message

--- src/modules/auth/dtos/verification-code-validation.dto.ts
+++ src/modules/auth/dtos/verification-code-validation.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 export class VerificationCodeValidationDto {
   @IsNotEmpty()
   @IsString()
-  @Length(6, 6)
+  @Length(6, 6, { message: 'Verification code must be 6 characters long' })
   code: string;
 
   @IsNotEmpty()
