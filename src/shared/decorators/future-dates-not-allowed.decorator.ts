35a23592dc8c321c1cf99265c0fb8edd18f35ecb
= <=>
2024-05-03 12:17:36 -0300

    cia-132: Create a date validation to not allow future dates

---
+++ src/shared/decorators/future-dates-not-allowed.decorator.ts
@@ -0,0 +1,27 @@
+import {
+  ValidationArguments,
+  ValidationOptions,
+  registerDecorator,
+} from 'class-validator';
+
+export function IsNotFutureDate(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'isNotFutureDate',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: any) {
+          const currentDate = new Date();
+          const inputDate = new Date(value);
+
+          return inputDate <= currentDate;
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} should not be a future date`;
+        },
+      },
+    });
+  };
+}
